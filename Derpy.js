e22f52ca916e13f6cbce64533cb7cc2cd3dd26e0
jjrambo97 <hondacaleb@gmail.com>
2013-02-24 06:29:22 -0500

    Create Derpy.js

---
+++ Derpy.js
@@ -0,0 +1,19 @@
+function addGlobalStyle(css){
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if(!head){
+        return;
+    }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+//changes the background
+addGlobalStyle('body, body {background: #080c0f url(http://dl.dropbox.com/u/61594284/Plug.Pony%20Backgrounds/Derpy.png) no-repeat;}');
+addGlobalStyle('#room-wheel, #room-wheel {height: 0;}');
+
+    setTimeout(function(){
+        RoomUser.audience.roomElements = [];
+    RoomUser.redraw();
+    }, 100);
