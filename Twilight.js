44c864c71821f6a465c330d6cd7a51794e572b77
jjrambo97 <hondacaleb@gmail.com>
2013-03-02 17:59:34 -0500

    Update Twilight.js

--- Twilight.js
+++ Twilight.js
@@ -13,7 +13,5 @@ function addGlobalStyle(css){
 addGlobalStyle('body, body {background: #000000 url(http://dl.dropbox.com/u/61594284/Plug.Pony%20Backgrounds/TS.png) no-repeat;}');
 addGlobalStyle('#room-wheel, #room-wheel {height: 0;}');
 
- setTimeout(function(){
         RoomUser.audience.roomElements = [];
     RoomUser.redraw();
-    }, 100);
