1a4852bacc065d51293ea1ad8a558867a7176a89
jjrambo97 <hondacaleb@gmail.com>
2013-03-02 17:57:57 -0500

    Update Derpy.js

--- Derpy.js
+++ Derpy.js
@@ -13,7 +13,5 @@ function addGlobalStyle(css){
 addGlobalStyle('body, body {background: #080c0f url(http://dl.dropbox.com/u/61594284/Plug.Pony%20Backgrounds/Derpy.png) no-repeat;}');
 addGlobalStyle('#room-wheel, #room-wheel {height: 0;}');
 
-    setTimeout(function(){
         RoomUser.audience.roomElements = [];
     RoomUser.redraw();
-    }, 100);
