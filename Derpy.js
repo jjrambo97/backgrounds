510f78bd72b10ac2624abc034101bad045dbc925
jjrambo97 <hondacaleb@gmail.com>
2013-08-16 23:04:50 -0400

    Update Derpy.js

--- Derpy.js
+++ Derpy.js
@@ -12,6 +12,3 @@ function addGlobalStyle(css){
 //changes the background
 addGlobalStyle('body, body {background: #080c0f url(http://dl.dropbox.com/u/61594284/Plug.Pony%20Backgrounds/Derpy.png) no-repeat;}');
 addGlobalStyle('#room-wheel, #room-wheel {height: 0;}');
-
-        RoomUser.audience.roomElements = [];
-    RoomUser.redraw();
